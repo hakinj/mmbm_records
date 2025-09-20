cc75ca30d79ac0383227d98915b8acfd33f4bdc1
Akinjide <akinjide19@gmail.com>
2025-09-20 20:16:49 +0100

    rename image

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ProductCard from "../utils/Products";
 //  
 const products = [
-  { id: 1, name: "Limited Tee", description: "Premium hoodie", price: 120, stock: 5, image: "/Image (1).png" },
-  { id: 2, name: "Limited Tee", description: "Exclusive cotton tee", price: 200, stock: 0, image: "/Image (2).png" },
-  { id: 3, name: "Custom Tshirt", description: "Hand-stitched denim", price: 200, stock: 3, image: "/Image (3).png" },
-  { id: 4, name: "Custom Tshirt", description: "Hand-stitched denim", price: 200, stock: 3, image: "/Image (4).png" },
-  { id: 5, name: "Artist Hoodie", description: "Hand-stitched denim", price: 200, stock: 3, image: "/Image (5).png" }
+  { id: 1, name: "Limited Tee", description: "Premium hoodie", price: 120, stock: 5, image: "/Image1.png" },
+  { id: 2, name: "Limited Tee", description: "Exclusive cotton tee", price: 200, stock: 0, image: "/Image2.png" },
+  { id: 3, name: "Custom Tshirt", description: "Hand-stitched denim", price: 200, stock: 3, image: "/Image3.png" },
+  { id: 4, name: "Custom Tshirt", description: "Hand-stitched denim", price: 200, stock: 3, image: "/Image4.png" },
+  { id: 5, name: "Artist Hoodie", description: "Hand-stitched denim", price: 200, stock: 3, image: "/Image5.png" }
 ];
 
 const ShopPage1: React.FC = () => (
